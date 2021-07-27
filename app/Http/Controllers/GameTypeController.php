c4aabfe55d0be9bb6b8c9b00ae1c87777cf826b0
sreeparnadas <sreeparnadas0675@gmail.com>
2021-07-27 09:46:18 +0530

    cpanel

--- app/Http/Controllers/GameTypeController.php
+++ app/Http/Controllers/GameTypeController.php
@@ -20,4 +20,16 @@ class GameTypeController extends Controller
     }
 
 
+    public function update_payout(Request $request){
+        $requestedData = (object)$request->json()->all();
+        $gameTypeId = $requestedData->gameTypeId;
+        $newPayout = $requestedData->newPayout;
+        $gameType = GameType::find($gameTypeId);
+        $gameType->payout = $newPayout;
+        $gameType->save();
+
+        return response()->json(['success'=>1,'data'=>$gameType], 200,[],JSON_NUMERIC_CHECK);
+    }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -91,11 +91,16 @@ Route::group(['middleware' => 'auth:sanctum'], function(){
     Route::get('terminals/{id}',[TerminalController::class, 'get_stockist_by_terminal_id']);
     Route::put('terminals/balance',[TerminalController::class, 'update_balance_to_terminal']);
 
-//    Admin reports
-
     Route::get('cPanel/barcodeReport', [CPanelReportController::class, 'barcode_wise_report']);
     Route::get('cPanel/barcodeReport/particulars/{id}', [CPanelReportController::class, 'get_barcode_report_particulars']);
+    Route::get('cPanel/barcodeReport/prizeValue/{id}', [CPanelReportController::class, 'get_prize_value_by_barcode']);
     Route::get('cPanel/customerSaleReport', [CPanelReportController::class, 'customer_sale_report']);
+    Route::get('terminal/barcodeReport',[TerminalReportController::class, 'barcode_wise_report_by_terminal']);
+
+
+    Route::put('terminal/resetPassword', [TerminalController::class, 'reset_terminal_password']);
+
+    Route::put('cPanel/game/payout',[GameTypeController::class, 'update_payout']);
 });
 
 
@@ -170,6 +175,8 @@ Route::group(array('prefix' => 'dev'), function() {
 
 
     Route::put('terminal/resetPassword', [TerminalController::class, 'reset_terminal_password']);
+
+    Route::put('cPanel/game/payout',[GameTypeController::class, 'update_payout']);
 
 });
 
