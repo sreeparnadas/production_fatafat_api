67d737af9350235acff85632a8e4437c96445488
mounitbhandari <tataibhandari@gmail.com>
2022-01-10 20:44:40 +0530

    working on api

--- routes/api.php
+++ routes/api.php
@@ -136,30 +136,30 @@ Route::group(array('prefix' => 'dev'), function() {
 
     Route::get('activateGame/{id}', [GameController::class, 'activate_game']);
 
-    Route::post('terminal/updateCancellation/{id}', [TerminalReportController::class, 'updateCancellationGameWise']);
-    Route::post('updateDrawOver', [CentralController::class, 'update_is_draw_over']);
+    // Route::post('terminal/updateCancellation/{id}', [TerminalReportController::class, 'updateCancellationGameWise']);
+    // Route::post('updateDrawOver', [CentralController::class, 'update_is_draw_over']);
 
-    Route::post('createAutoResult/{id}', [CentralController::class, 'createResult']);
+    // Route::post('createAutoResult/{id}', [CentralController::class, 'createResult']);
 
-    Route::get('getGame', [GameController::class, 'getGame']);
+    // Route::get('getGame', [GameController::class, 'getGame']);
 
     Route::post('getResultByDate', [ResultMasterController::class, 'get_result_by_date']);
 
     Route::post('cPanel/barcodeReportByDate', [CPanelReportController::class, 'barcode_wise_report_by_date']);
-    Route::post('stockist/customerSaleReports', [StockistController::class, 'customer_sale_reports']);
+    // Route::post('stockist/customerSaleReports', [StockistController::class, 'customer_sale_reports']);
 
-    Route::post('stockist/barcodeReportByDate', [StockistController::class, 'barcode_wise_report_by_date']);
+    // Route::post('stockist/barcodeReportByDate', [StockistController::class, 'barcode_wise_report_by_date']);
 
-    Route::get("users",[UserController::class,'getAllUsers']);
-    Route::patch("users",[UserController::class,'update']);
+    // Route::get("users",[UserController::class,'getAllUsers']);
+    // Route::patch("users",[UserController::class,'update']);
 
     //single_numbers
-    Route::get("singleNumbers",[SingleNumberController::class,'index']);
+    // Route::get("singleNumbers",[SingleNumberController::class,'index']);
 
     //number_combinations
-    Route::get("numberCombinations",[NumberCombinationController::class,'index']);
-    Route::get("numberCombinations/number/{number}",[NumberCombinationController::class,'getNumbersBySingleNumber']);
-    Route::get("numberCombinations/matrix",[NumberCombinationController::class,'getAllInMatrix']);
+    // Route::get("numberCombinations",[NumberCombinationController::class,'index']);
+    // Route::get("numberCombinations/number/{number}",[NumberCombinationController::class,'getNumbersBySingleNumber']);
+    // Route::get("numberCombinations/matrix",[NumberCombinationController::class,'getAllInMatrix']);
 
     //draw_masters
     Route::get('drawTimes',[DrawMasterController::class,'index']);
@@ -169,13 +169,13 @@ Route::group(array('prefix' => 'dev'), function() {
     Route::get('drawTimes/dates/{id}',[DrawMasterController::class,'get_incomplete_games_by_date']);
 
     //game_types
-    Route::get('gameTypes',[GameTypeController::class,'index']);
+    // Route::get('gameTypes',[GameTypeController::class,'index']);
 
     //play_masters
-    Route::post('buyTicket',[PlayController::class,'save_play_details']);
+    // Route::post('buyTicket',[PlayController::class,'save_play_details']);
 
     //game
-    Route::get('playDetails/playId/{id}',[PlayMasterController::class,'get_play_details_by_play_master_id']);
+    // Route::get('playDetails/playId/{id}',[PlayMasterController::class,'get_play_details_by_play_master_id']);
 
     //result_masters
     Route::get('results',[ResultMasterController::class, 'get_results']);
@@ -186,30 +186,30 @@ Route::group(array('prefix' => 'dev'), function() {
 
     //manual_result
 
-    Route::post('manualResult',[ManualResultController::class, 'save_manual_result']);
+    // Route::post('manualResult',[ManualResultController::class, 'save_manual_result']);
 
 
     //test
-    Route::get('test',[Test::class, 'index']);
+    // Route::get('test',[Test::class, 'index']);
 
 
-    Route::get('stockists',[StockistController::class, 'get_all_stockists']);
-    Route::get('stockists/{id}',[StockistController::class, 'get_stockist']);
-    Route::post('stockists',[StockistController::class, 'create_stockist']);
-    Route::put('stockists',[StockistController::class, 'update_stockist']);
-    Route::put('stockists/balance',[StockistController::class, 'update_balance_to_stockist']);
+    // Route::get('stockists',[StockistController::class, 'get_all_stockists']);
+    // Route::get('stockists/{id}',[StockistController::class, 'get_stockist']);
+    // Route::post('stockists',[StockistController::class, 'create_stockist']);
+    // Route::put('stockists',[StockistController::class, 'update_stockist']);
+    // Route::put('stockists/balance',[StockistController::class, 'update_balance_to_stockist']);
 
 
-    Route::get('terminals',[TerminalController::class, 'get_all_terminals']);
-    Route::post('terminals',[TerminalController::class, 'create_terminal']);
-    Route::put('terminals',[TerminalController::class, 'update_terminal']);
-    Route::get('terminals/{id}',[TerminalController::class, 'get_stockist_by_terminal_id']);
-    Route::put('terminals/balance',[TerminalController::class, 'update_balance_to_terminal']);
+    // Route::get('terminals',[TerminalController::class, 'get_all_terminals']);
+    // Route::post('terminals',[TerminalController::class, 'create_terminal']);
+    // Route::put('terminals',[TerminalController::class, 'update_terminal']);
+    // Route::get('terminals/{id}',[TerminalController::class, 'get_stockist_by_terminal_id']);
+    // Route::put('terminals/balance',[TerminalController::class, 'update_balance_to_terminal']);
 
 
-    Route::post('createAutoResult', [CentralController::class, 'createResult']);
+    // Route::post('createAutoResult', [CentralController::class, 'createResult']);
 
-    Route::post('autoResult', [ResultMasterController::class, 'save_auto_result']);
+    // Route::post('autoResult', [ResultMasterController::class, 'save_auto_result']);
 
     Route::get('nextDrawId', [NextGameDrawController::class, 'getNextDrawIdOnly']);
 
@@ -217,19 +217,19 @@ Route::group(array('prefix' => 'dev'), function() {
     Route::get('cPanel/barcodeReport', [CPanelReportController::class, 'barcode_wise_report']);
     Route::get('cPanel/barcodeReport/particulars/{id}', [CPanelReportController::class, 'get_barcode_report_particulars']);
     Route::get('cPanel/barcodeReport/prizeValue/{id}', [CPanelReportController::class, 'get_prize_value_by_barcode']);
-    Route::get('cPanel/customerSaleReport', [CPanelReportController::class, 'customer_sale_report']);
-    Route::post('cPanel/customerSaleReports', [CPanelReportController::class, 'customer_sale_reports']);
-    Route::post('terminal/terminal_sale_reports', [TerminalReportController::class, 'terminal_sale_reports']);
-    Route::post('terminal/barcodeReport',[TerminalReportController::class, 'barcode_wise_report_by_terminal']);
+    // Route::get('cPanel/customerSaleReport', [CPanelReportController::class, 'customer_sale_report']);
+    // Route::post('cPanel/customerSaleReports', [CPanelReportController::class, 'customer_sale_reports']);
+    // Route::post('terminal/terminal_sale_reports', [TerminalReportController::class, 'terminal_sale_reports']);
+    // Route::post('terminal/barcodeReport',[TerminalReportController::class, 'barcode_wise_report_by_terminal']);
 
 
-    Route::put('terminal/resetPassword', [TerminalController::class, 'reset_terminal_password']);
+    // Route::put('terminal/resetPassword', [TerminalController::class, 'reset_terminal_password']);
 
-    Route::put('cPanel/game/payout',[GameTypeController::class, 'update_payout']);
+    // Route::put('cPanel/game/payout',[GameTypeController::class, 'update_payout']);
 
     Route::get('getGame', [GameController::class, 'getGame']);
 
-    Route::get('updateAutoGenerate/{id}', [GameController::class, 'update_auto_generate']);
+    // Route::get('updateAutoGenerate/{id}', [GameController::class, 'update_auto_generate']);
 
 
 });
