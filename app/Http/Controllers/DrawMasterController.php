914e3373176ca85f7f009610ca6ab51732119410
priyamghosh1 <priyamghosh73@gmail.com>
2022-01-14 21:01:11 +0530

    manual result updated

--- app/Http/Controllers/DrawMasterController.php
+++ app/Http/Controllers/DrawMasterController.php
@@ -27,13 +27,14 @@ class DrawMasterController extends Controller
 
     public function get_incomplete_games_by_date($id){
         $test = Carbon::today();
-        $result = DrawMaster::whereDoesnthave('result_masters', function($q) use ($test) {
-            $q->where('game_date', '=', $test);
-        })
+//        $result = DrawMaster::whereDoesnthave('result_masters', function($q) use ($test) {
+//            $q->where('game_date', '=', $test);
+//        })
 //            ->whereDoesnthave('manual_results', function($q) use ($test) {
 //            $q->where(DB::raw('date(created_at)'), '=', $test);
 //        })
-            ->whereGameId($id)
+        $result = DrawMaster::
+            whereGameId($id)
             ->get();
         return response()->json(['success'=>1,'data'=>DrawMasterResource::collection($result)], 200,[],JSON_NUMERIC_CHECK);
     }
